6391966182460936f94d2ce5d6a03bd28abdc635
gouguoyin <245629560@qq.com>
2025-01-22 15:52:38 +0800

    feat(sign-js): 创建 gocmf 签名库
    - 新增 sign-js 项目，实现签名生成功能
    - 添加项目配置文件和基本结构
    - 实现签名生成算法，支持参数排序和 MD5 哈希计算
    - 创建 README 文件，说明项目用途和使用方法

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# sign-js
-signature library for gocmf
+# gocmf-sign
+
+a signature library for gocmf.
+
+## Node.js (Install)
+
+Requirements:
+
+- Node.js
+- npm (Node.js package manager)
+
+```bash
+npm install gocmf-sign
+```
+
+### Usage
+
+ES6 import for typical API call signing use case:
+
+```javascript
+import sign from 'gocmf-sign'
+const params = {
+    name: 'gouguoyin',
+    age: 18,
+    sex: ''
+}
+const secretKey = 'gocmf'
+const timestamp = Date.now()
+const signature = sign.generate(params, secretKey, timestamp);
+```
---
+++ src/index.js
@@ -0,0 +1,37 @@
+import crypto from 'crypto-js'
+
+const sign = {
+    /**
+     * 生成签名
+     * @param params 请求参数
+     * @param secretKey 密钥
+     * @param timestamp 时间戳
+     * @returns 签名
+     */
+    generate(params: Record<string, any>, secretKey: string, timestamp: number): string {
+        if (!params) {
+            return ''
+        }
+        // 对参数进行 key 排序
+        const sortedParams = {}
+        Object.keys(params)
+            .sort()
+            .forEach((key) => {
+                if (typeof params[key] === 'number') {
+                    sortedParams[key] = params[key]
+                }
+                if (typeof params[key] === 'string' && params[key].length > 0) {
+                    sortedParams[key] = params[key]
+                }
+            })
+        // 生成签名
+        const signature =
+            Object.keys(sortedParams)
+                .map((key) => `${key}=${sortedParams[key]}&`)
+                .join('') + `secretKey=${secretKey}&timestamp=${timestamp}`
+        // 使用 MD5 算法对签名进行哈希计算
+        return crypto.MD5(signature).toString()
+    }
+}
+
+export default sign
